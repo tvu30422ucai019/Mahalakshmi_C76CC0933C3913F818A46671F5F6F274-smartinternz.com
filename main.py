efd4c6b1109697da31b4253bf063bf2cf2052b53
tvu30422ucai019 <144305427+tvu30422ucai019@users.noreply.github.com>
2023-09-07 09:59:02 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+def factorial(n):
+  if n == 1:
+    return n
+  else:
+    return n*factorial(n-1)
+num=int(input("enter any number:"))
+if num<0:
+  print("factorial does not exist for negative numbers")
+elif num==0:
+  print("the factorial of 0 is 1")
+else:
+  print("the factorial of",num,"is",factorial(num))
